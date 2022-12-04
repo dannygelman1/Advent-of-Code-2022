dd74ac0864f4fbbca9bc4ea62fb2f5d1cce9683e
Danny Gelman <45411340+dannygelman1@users.noreply.github.com>
2022-12-04 00:41:18 -0500

    Update advent1.py

--- advent1/advent1.py
+++ advent1/advent1.py
@@ -1,15 +1,15 @@
 f = open("./advent1.txt", "r")
 
-listNumbers = f.read().split("\n")
-arrayMaxCalories = [float("-inf"), float("-inf"), float("-inf")]
+calories = f.read().split("\n")
+maxCalories = [float("-inf"), float("-inf"), float("-inf")]
 sumCalories = 0
-for number in listNumbers:
+for number in calories:
   if (number!=""):
     sumCalories += int(number)
   else:
-    if sumCalories> min(arrayMaxCalories):
-      index = arrayMaxCalories.index(min(arrayMaxCalories))
-      arrayMaxCalories[index] = sumCalories
+    if sumCalories> min(maxCalories):
+      index = maxCalories.index(min(maxCalories))
+      maxCalories[index] = sumCalories
     sumCalories = 0
-print("Part 1: ", max(arrayMaxCalories))
-print("Part 2: ", sum(arrayMaxCalories))
+print("Part 1: ", max(maxCalories))
+print("Part 2: ", sum(maxCalories))
